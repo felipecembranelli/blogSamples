41cec45001a95b54aa70d281b9dcda96a5197859
felipecembranelli <;FE307609>
2014-12-17 17:23:16 -0200

    adicionei o json 2 sample

---
+++ JSONSample2/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace JSONSample2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            LoadJson();
+        }
+
+        public async void LoadJson()
+        {
+            // Load json file
+            string fileContent;
+
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var file = await folder.GetFileAsync("jobs.json");
+
+            using (StreamReader sRead = new StreamReader(await file.OpenStreamForReadAsync()))
+                fileContent = await sRead.ReadToEndAsync();
+
+            // Parse
+            LinkedinJobList items = JsonConvert.DeserializeObject<LinkedinJobList>(fileContent);
+        }
+    }
+}
